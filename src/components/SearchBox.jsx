2ae9c21dd5aca64a98ac54faaa9fea8a86dbf851
sarvgyaverma027@gmail.com <sarvgyaverma027@gmail.com>
2024-05-31 19:58:15 +0530

    completed the searchbox

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
+import { RxCross2 } from 'react-icons/rx'
+import { BsFillMicFill } from 'react-icons/bs'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { useRouter, useSearchParams } from 'next/navigation'
+
 
 function SearchBox() {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const searchTerm = searchParams.get('searchTerm')
+  const [term, setTerm] = useState(searchTerm || '')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!term.trim)
+    router.push(`/search/web?searchTerm=${term}`)
+  }
+
   return (
-    <div>
-      searchbox
+    <div className="relative">
+      <div className="absolute inset-1 bg-gray-600 bg-gradient-to-r from-[#14a6ab] to-[#947d37] blur-[100px] opacity-80 rounded-md z-0"></div>
+      <form onSubmit={handleSubmit} className="flex border border-gray-300 rounded-xl shadow-lg px-4 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center z-10 hover:shadow-gray-400 focus-within:shadow-md transition-shadow">
+        <input
+          type="text"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+          className="z-10 bg-transparent text-[#CBE4DE] rounded-md w-full focus:outline-none 
+          "
+        />
+        <RxCross2 onClick={() => setTerm('')} className="text-2xl text-gray-300 cursor-pointer sm:mr-2 z-10" />
+        <BsFillMicFill className="hidden sm:inline-flex text-4xl text-[#CBE4DE] border-l-2 border-gray-300 mr-3 pl-4 z-10" />
+        <AiOutlineSearch className="text-2xl hidden sm:inline-flex text-[#CBE4DE] cursor-pointer z-10"
+        onClick={handleSubmit} />
+      </form>
     </div>
   )
 }
--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -17,17 +17,17 @@ function SearchHeader() {
     <header className={`${nunito.className}  `}>
      <div className='m-2 flex p-4 items-center justify-between '>
       <Link href='/'>
-      <Image alt="logo" src="/HeaderLogo.png" className="" width={150} height={150} priority={true} />
+      <Image alt="logo" src="/HeaderLogo.png" className="" width={180} height={150} priority={true} />
       </Link>
       <div className='flex-1 '>
         <SearchBox />
       </div>
-      <div className='hidden md:inline-flex space-x-2 mr-2 '>
+      <div className='hidden md:inline-flex space-x-2 mr-4 gap-3   '>
         <RiSettings3Line className='text-[#CBE4DE] text-md text-[28px] hover:bg-gray-600 rounded-full transition duration-400 delay-10 ease-in-out '/>
         <TbGridDots className='text-[#CBE4DE] text-md text-[28px] hover:bg-gray-600 rounded-full transition duration-400 delay-10 ease-in-out '/>
 
       </div>
-        <button className='ml-2 bg-transparent hover:bg-[#149ba0] text-white  hover:text-white py-2 px-4 border border-[#149ba0] hover:border-transparent rounded-full text-xs transition duration-400 delay-10 ease-in-out'>Sign in</button>
+        <button className=' bg-transparent hover:bg-[#149ba0] text-white  hover:text-white py-2 px-5 border border-[#149ba0] hover:border-transparent rounded-full text-xs transition duration-400 delay-10 ease-in-out'>Sign in</button>
      </div>
     </header>
   )

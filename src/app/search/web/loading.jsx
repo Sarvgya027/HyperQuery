8a59d17580d70b8066130c6d4d7b3abb8b59e6b0
sarvgyaverma027@gmail.com <sarvgyaverma027@gmail.com>
2024-06-01 10:11:12 +0530

    added loding page

---
+++ src/app/search/web/loading.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function loading() {
+ return (
+    <>
+    <div className='mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse'>
+      <div className='h-2.5 w-48 bg-gray-700 rounded-xl mb-2.5'></div>
+      <div className='h-3.5 w-[360px] bg-gray-500 rounded-xl mb-2.5'></div>
+      <div className='h-2.5 w-[560px] bg-gray-500 rounded-xl mb-2.5'></div>
+      <div className='h-2.5 w-[530px] bg-gray-500 rounded-xl mb-2.5'></div>
+    </div>
+    <div className='mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse'>
+      <div className='h-2.5 w-48 bg-gray-700 rounded-xl mb-2.5'></div>
+      <div className='h-3.5 w-[360px] bg-gray-500 rounded-xl mb-2.5'></div>
+      <div className='h-2.5 w-[560px] bg-gray-500 rounded-xl mb-2.5'></div>
+      <div className='h-2.5 w-[530px] bg-gray-500 rounded-xl mb-2.5'></div>
+    </div>
+    <div className='mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse'>
+      <div className='h-2.5 w-48 bg-gray-700 rounded-xl mb-2.5'></div>
+      <div className='h-3.5 w-[360px] bg-gray-500 rounded-xl mb-2.5'></div>
+      <div className='h-2.5 w-[560px] bg-gray-500 rounded-xl mb-2.5'></div>
+      <div className='h-2.5 w-[530px] bg-gray-500 rounded-xl mb-2.5'></div>
+    </div>
+    </>
+  )
+}
+
+export default loading;
